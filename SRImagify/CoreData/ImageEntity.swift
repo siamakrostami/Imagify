d8e361776f87ba1a0e45310119664387a3cfe480
Siamak Rostami <44254012+siamakrostami@users.noreply.github.com>
2021-12-12 14:59:34 +0330

    -removed unused code

--- SRImagify/CoreData/ImageEntity.swift
+++ SRImagify/CoreData/ImageEntity.swift
@@ -31,23 +31,6 @@ extension ImageEntity{
     @NSManaged public  var download_url : String?
     @NSManaged public var size : Int64
     
-    static func convertDictionaryToModel(dictionary : Dictionary<String,Any>) -> ImageEntity{
-        let imageObject = ImageEntity()
-        imageObject.id = dictionary["id"] as? String
-        imageObject.user_id = dictionary["user_id"] as? String
-        imageObject.media_type = dictionary["media_type"] as? String
-        imageObject.created_at = dictionary["created_at"] as? String
-        imageObject.taken_at = dictionary["taken_at"] as? String
-        imageObject.guessed_taken_at = dictionary["guessed_taken_at"] as? String
-        imageObject.md5sum = dictionary["md5sum"] as? String
-        imageObject.content_type = dictionary["content_type"] as? String
-        imageObject.video = dictionary["video"] as? String
-        imageObject.filename = dictionary["filename"] as? String
-        imageObject.thumbnail_url = dictionary["thumbnail_url"] as? String
-        imageObject.download_url = dictionary["download_url"] as? String
-        imageObject.size = dictionary["size"] as? Int64 ?? 0
-        return imageObject
-    }
 }
 
 
